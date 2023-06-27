b12615f5c1c774104a3c0d9a2b2ea1574a7222ce
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-06-27 16:58:22 +0530

    Update Blockchain.py

--- Creating_Blockchain/Blockchain.py
+++ Creating_Blockchain/Blockchain.py
@@ -90,5 +90,14 @@ def get_chain():
                'length': len(blockchain.chain)}
     return jsonify(respose), 200
 
+# Checking if the blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The blockchain is valid.'}
+    else:
+        response = {'message': 'The blockchain is not valid.'}
+    return jsonify(response), 200
 #Running the app
 app.run(host = '0.0.0.0', port = 5000)

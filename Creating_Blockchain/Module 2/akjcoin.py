131cbc312211045fde2a76bae05e8cdb196744f8
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-07-12 00:22:41 +0530

    Update akjcoin.py

--- Creating_Blockchain/Module 2/akjcoin.py
+++ Creating_Blockchain/Module 2/akjcoin.py
@@ -150,6 +150,30 @@ def add_transaction():
     return jsonify(response), 201
 #Decentralising our blockchain
 
+# Connecting new nodes 
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The akjcoin blockchain now contains the following nodes:',
+                'total_nodes':list(blockchain.nodes)}
+    return jsonify(response), 201
+
+#Replacing the chain by the longest chain if needed
+@app.route('/replace_chain', methods = ['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'The nodes had different chains so the chain was replaced by the longer one',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one.',
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
 
 #Running the app
 app.run(host = '0.0.0.0', port = 5000)
